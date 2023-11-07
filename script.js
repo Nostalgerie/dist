64f2b38db9a7335b5c04fe5165e68c0e1a394e84
Nostalgerie <150078355+Nostalgerie@users.noreply.github.com>
2023-11-07 23:00:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -168,11 +168,8 @@ class Slider extends React.Component {
     if (this.state.current !== index) {
       this.setState({
         current: index });
-    const url = this.props.slides[index].url;
-    if (url) {
-      window.location.href = url;
-     }
-   }
+
+    }
   }
 
   render() {
