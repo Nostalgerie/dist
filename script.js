4d339d5bdf0dbcb3e6a80e20f3ea4b154d4249b1
Nostalgerie <150078355+Nostalgerie@users.noreply.github.com>
2023-11-07 16:03:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Hammer from 'hammerjs'; // Make sure you have Hammer.js imported
-
 const slideData = [
-  {
-    index: 0,
-    headline: 'Carmen R',
-    button: 'Explore',
-    src: 'https://i.imgur.com/5Lf2kgz.jpg'
-  },
-  {
-    index: 1,
-    headline: 'Lady',
-    button: 'Explore',
-    src: 'https://i.imgur.com/kPrsCl9.jpg'
-  },
-  {
-    index: 2,
-    headline: 'Dana R',
-    button: 'Explore',
-    src: 'https://i.imgur.com/M2VwVn2.jpg'
-  },
-  {
-    index: 3,
-    headline: 'Dana B',
-    button: 'Explore',
-    src: 'https://i.imgur.com/4YtBya0.jpg'
-  },
-  {
-    index: 4,
-    headline: 'Cora W',
-    button: 'Explore',
-    src: 'https://i.imgur.com/T16xmnq.jpg'
-  },
-  {
-    index: 5,
-    headline: 'Miss',
-    button: 'Explore',
-    src: 'https://i.imgur.com/zj0kpFj.jpg'
-  }
-];
+{
+  index: 0,
+  headline: 'Carmen R',
+  button: 'Explore',
+  src: 'https://i.imgur.com/5Lf2kgz.jpg' },
+
+{
+  index: 1,
+  headline: 'Lady',
+  button: 'Explore',
+  src: 'https://i.imgur.com/kPrsCl9.jpg' },
+
+{
+  index: 2,
+  headline: 'Dana R',
+  button: 'Explore',
+  src: 'https://i.imgur.com/M2VwVn2.jpg' },
+
+{
+  index: 3,
+  headline: 'Dana B',
+  button: 'Explore',
+  src: 'https://i.imgur.com/4YtBya0.jpg' },
+
+{
+  index: 4,
+  headline: 'Cora W',
+  button: 'Explore',
+  src: 'https://i.imgur.com/T16xmnq.jpg' },
+
+{
+  index: 5,
+  headline: 'Miss',
+  button: 'Explore',
+  src: 'https://i.imgur.com/zj0kpFj.jpg' }];
+
+
+
+
+// =========================
+// Slide
+// =========================
 
 class Slide extends React.Component {
   constructor(props) {
@@ -57,7 +58,7 @@ class Slide extends React.Component {
     const r = el.getBoundingClientRect();
 
     el.style.setProperty('--x', event.clientX - (r.left + Math.floor(r.width / 2)));
-    el.style.setProperty('--y', event.clientY - (r.top + Math floor(r.height / 2)));
+    el.style.setProperty('--y', event.clientY - (r.top + Math.floor(r.height / 2)));
   }
 
   handleMouseLeave(event) {
@@ -78,48 +79,55 @@ class Slide extends React.Component {
     const current = this.props.current;
     let classNames = 'slide';
 
-    if (current === index) classNames += ' slide--current';
-    else if (current - 1 === index) classNames += ' slide--previous';
-    else if (current + 1 === index) classNames += ' slide--next';
+    if (current === index) classNames += ' slide--current';else
+    if (current - 1 === index) classNames += ' slide--previous';else
+    if (current + 1 === index) classNames += ' slide--next';
 
-    return (
-      <li
-        ref={this.slide}
-        className={classNames}
-        onClick={this.handleSlideClick}
-        onMouseMove={this.handleMouseMove}
-        onMouseLeave={this.handleMouseLeave}
-      >
-        <div className="slide__image-wrapper">
-          <img
-            className="slide__image"
-            alt={headline}
-            src={src}
-            onLoad={this.imageLoaded}
-          />
-        </div>
-        <article className="slide__content">
-          <h2 className="slide__headline">{headline}</h2>
-          <button className="slide__action btn">{button}</button>
-        </article>
-      </li>
-    );
-  }
-}
+    return /*#__PURE__*/(
+      React.createElement("li", {
+        ref: this.slide,
+        className: classNames,
+        onClick: this.handleSlideClick,
+        onMouseMove: this.handleMouseMove,
+        onMouseLeave: this.handleMouseLeave }, /*#__PURE__*/
+
+      React.createElement("div", { className: "slide__image-wrapper" }, /*#__PURE__*/
+      React.createElement("img", {
+        className: "slide__image",
+        alt: headline,
+        src: src,
+        onLoad: this.imageLoaded })), /*#__PURE__*/
+
+
+
+      React.createElement("article", { className: "slide__content" }, /*#__PURE__*/
+      React.createElement("h2", { className: "slide__headline" }, headline), /*#__PURE__*/
+      React.createElement("button", { className: "slide__action btn" }, button))));
+
+
+
+  }}
+
+
+
+// =========================
+// Slider control
+// =========================
 
 const SliderControl = ({ type, title, handleClick }) => {
-  return (
-    <button
-      className={`btn btn--${type}`}
-      title={title}
-      onClick={handleClick}
-    >
-      <svg className="icon" viewBox="0 0 24 24">
-        <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-      </svg>
-    </button>
-  );
+  return /*#__PURE__*/(
+    React.createElement("button", { className: `btn btn--${type}`, title: title, onClick: handleClick }, /*#__PURE__*/
+    React.createElement("svg", { className: "icon", viewBox: "0 0 24 24" }, /*#__PURE__*/
+    React.createElement("path", { d: "M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" }))));
+
+
+
 };
+
+
+// =========================
+// Slider
+// =========================
 
 class Slider extends React.Component {
   constructor(props) {
@@ -135,79 +143,72 @@ class Slider extends React.Component {
     const previous = this.state.current - 1;
 
     this.setState({
-      current: previous < 0 ? this.props.slides.length - 1 : previous,
-    });
+      current: previous < 0 ?
+      this.props.slides.length - 1 :
+      previous });
+
   }
 
   handleNextClick() {
     const next = this.state.current + 1;
 
     this.setState({
-      current: next === this.props.slides.length ? 0 : next,
-    });
+      current: next === this.props.slides.length ?
+      0 :
+      next });
+
   }
 
   handleSlideClick(index) {
     if (this.state.current !== index) {
       this.setState({
-        current: index,
-      });
+        current: index });
+
     }
-  }
-
-  componentDidMount() {
-    // Add Hammer.js swipe event handling
-    this.hammer = new Hammer(this.sliderRef.current);
-    this.hammer.on("swipeleft", this.handleNextClick);
-    this.hammer.on("swiperight", this.handlePreviousClick);
-  }
-
-  componentWillUnmount() {
-    // Remove Hammer.js event listeners when the component unmounts
-    this.hammer.off("swipeleft", this.handleNextClick);
-    this.hammer.off("swiperight", this.handlePreviousClick);
   }
 
   render() {
     const { current, direction } = this.state;
     const { slides, heading } = this.props;
-    const headingId = `slider-heading__${heading.replace(/\s+/g, "-").toLowerCase()}`;
+    const headingId = `slider-heading__${heading.replace(/\s+/g, '-').toLowerCase()}`;
     const wrapperTransform = {
-      transform: `translateX(-${current * (100 / slides.length)}%)`,
-    };
+      'transform': `translateX(-${current * (100 / slides.length)}%)` };
 
-    return (
-      <div className="slider" aria-labelledby={headingId}>
-        <ul className="slider__wrapper" style={wrapperTransform} ref={this.sliderRef}>
-          <h3 id={headingId} className="visuallyhidden">
-            {heading}
-          </h3>
 
-          {slides.map((slide) => (
-            <Slide
-              key={slide.index}
-              slide={slide}
-              current={current}
-              handleSlideClick={this.handleSlideClick}
-            />
-          ))}
-        </ul>
+    return /*#__PURE__*/(
+      React.createElement("div", { className: "slider", "aria-labelledby": headingId }, /*#__PURE__*/
+      React.createElement("ul", { className: "slider__wrapper", style: wrapperTransform }, /*#__PURE__*/
+      React.createElement("h3", { id: headingId, class: "visuallyhidden" }, heading),
 
-        <div className="slider__controls">
-          <SliderControl
-            type="previous"
-            title="Go to previous slide"
-            handleClick={this.handlePreviousClick}
-          />
-          <SliderControl
-            type="next"
-            title="Go to next slide"
-            handleClick={this.handleNextClick}
-          />
-        </div>
-      </div>
-    );
-  }
-}
+      slides.map(slide => {
+        return /*#__PURE__*/(
+          React.createElement(Slide, {
+            key: slide.index,
+            slide: slide,
+            current: current,
+            handleSlideClick: this.handleSlideClick }));
 
-ReactDOM.render(<Slider heading="Example Slider" slides={slideData} />, document.getElementById('app'));
+
+      })), /*#__PURE__*/
+
+
+      React.createElement("div", { className: "slider__controls" }, /*#__PURE__*/
+      React.createElement(SliderControl, {
+        type: "previous",
+        title: "Go to previous slide",
+        handleClick: this.handlePreviousClick }), /*#__PURE__*/
+
+
+      React.createElement(SliderControl, {
+        type: "next",
+        title: "Go to next slide",
+        handleClick: this.handleNextClick }))));
+
+
+
+
+  }}
+
+
+
+ReactDOM.render( /*#__PURE__*/React.createElement(Slider, { heading: "Example Slider", slides: slideData }), document.getElementById('app'));
