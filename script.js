6eb60887932348972c65f617a20e988df7e662cd
Nostalgerie <150078355+Nostalgerie@users.noreply.github.com>
2023-11-12 23:04:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ class Slide extends React.Component {
 
       React.createElement("article", { className: "slide__content" }, /*#__PURE__*/
       React.createElement("h2", { className: "slide__headline" }, headline), /*#__PURE__*/
-      React.createElement("button", { className: "slide__action btn", onClick: this.handleButtonClick }, button)
+      React.createElement("button", { className: "slide__action btn", onClick: this.handleButtonClick }, button),
 
 
 
